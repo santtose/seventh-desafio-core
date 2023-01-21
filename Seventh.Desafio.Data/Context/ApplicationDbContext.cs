ce1ec33c0398dda2560d5e42d835ebaf383936dc
santtose <santtose@outlook.com>
2023-01-21 17:08:40 -0300

    criacao contexto e repository pattern

---
+++ Seventh.Desafio.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Seventh.Desafio.Business.Entidades;
+using System.Linq;
+
+namespace Seventh.Desafio.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public virtual DbSet<Servidor> Servidores { get; set; }
+        public virtual DbSet<Video> Videos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Desabilita o Cascate Delete
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

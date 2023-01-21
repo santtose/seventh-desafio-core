fe46ad7bd7993f0cb54259560ec1e92454aae260
santtose <santtose@outlook.com>
2023-01-21 17:20:03 -0300

    configurando IOC

---
+++ Seventh.Desafio.Presentation/Configuration/DepedencyInjectionConfig.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Seventh.Desafio.Data.Context;
+
+namespace Seventh.Desafio.Presentation.Configuration
+{
+    public static class DepedencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
--- Seventh.Desafio.Presentation/Startup.cs
+++ Seventh.Desafio.Presentation/Startup.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Seventh.Desafio.Data.Context;
+using Seventh.Desafio.Presentation.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,14 +16,27 @@ namespace Seventh.Desafio.Presentation
 {
     public class Startup
     {
+        public static IConfiguration Configuration { get; private set; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddControllers();
+            services.ResolveDependencies();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {

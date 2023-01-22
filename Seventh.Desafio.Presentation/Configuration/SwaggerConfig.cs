ce9a74d2086572a36547b850b7b7c756d351d0bb
santtose <santtose@outlook.com>
2023-01-22 14:56:20 -0300

    adicionando swagger

---
+++ Seventh.Desafio.Presentation/Configuration/SwaggerConfig.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Seventh.Desafio.Presentation.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "Seventh Desafio",
+                    Description = "Desafio para vaga de dev da Seventh.",
+                    Contact = new OpenApiContact() { Name = "", Email = "" },
+                });
+            });
+
+            return services;
+        }
+        public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
+            return app;
+        }
+    }
+}
